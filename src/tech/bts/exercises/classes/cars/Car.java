94d71a64c1e7e1b58ea5bbf03ee7229b63fc535f
fmaylinch <ferranmaylinch@gmail.com>
2018-09-12 13:14:32 +0200

    Added Car constructor

--- src/tech/bts/exercises/classes/cars/Car.java
+++ src/tech/bts/exercises/classes/cars/Car.java
@@ -7,6 +7,10 @@ public class Car {
     int speed;
 
     // Constructor
+    public Car (String model, int speed) {
+        this.model = model;
+        this.speed = speed;
+    }
 
     // Methods
 }
--- src/tech/bts/exercises/classes/cars/Main.java
+++ src/tech/bts/exercises/classes/cars/Main.java
@@ -4,13 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Car ferrari = new Car();
-        ferrari.model = "Ferrari GTO";
-        ferrari.speed = 200;
+        Car ferrari = new Car("Ferrari GTO", 200);
 
-        Car audi = new Car();
-        audi.model = "Audi A8";
-        audi.speed = 150;
+        Car audi = new Car("Audi A8", 150);
 
         System.out.println(ferrari.model + " driving at " + ferrari.speed + "km/h");
         System.out.println(audi.model + " driving at " + audi.speed + "km/h");
