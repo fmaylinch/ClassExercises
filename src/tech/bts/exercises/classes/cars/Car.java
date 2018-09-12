f794908047de7c29dbac5cc6a78664765c4b69fc
fmaylinch <ferranmaylinch@gmail.com>
2018-09-12 15:20:32 +0200

    Add accelerate method

--- src/tech/bts/exercises/classes/cars/Car.java
+++ src/tech/bts/exercises/classes/cars/Car.java
@@ -14,6 +14,15 @@ public class Car {
 
     // Methods / functions
 
+    public void accelerate(int amount) {
+
+        this.speed = this.speed + amount;
+
+        if (this.speed > 300) {
+            this.speed = 300;
+        }
+    }
+
     public void printStatus() {
         System.out.println(this.model + " driving at " + this.speed + "km/h");
     }
--- src/tech/bts/exercises/classes/cars/Main.java
+++ src/tech/bts/exercises/classes/cars/Main.java
@@ -7,7 +7,10 @@ public class Main {
         Car ferrari = new Car("Ferrari GTO");
         Car audi = new Car("Audi A8");
 
-        ferrari.speed = 200;
+        ferrari.accelerate(100);
+        ferrari.accelerate(100);
+        ferrari.accelerate(99);
+        ferrari.accelerate(50);
 
         ferrari.printStatus();
         audi.printStatus();
